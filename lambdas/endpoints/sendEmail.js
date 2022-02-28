74c1f14d5a525f8effe4e9a974cd542d9b6536f6
Bezll <jabez1992@hotmail.com>
2022-02-28 11:36:00 +0000

    added a send email endpoint

---
+++ lambdas/endpoints/sendEmail.js
@@ -0,0 +1,37 @@
+const AWS = require("aws-sdk");
+const { sendResponse200, sendResponse400 } = require("../common/api_responses");
+
+const SES = new AWS.SES();
+
+exports.handler = async (event) => {
+	console.log("event", event);
+
+	const { to, from, subject, text } = JSON.parse(event.body);
+
+	if (!to || !from || !subject || !text) {
+		return sendResponse400({
+			message: "to, from, subject and text are all required in the body",
+		});
+	}
+
+	const params = {
+		Destination: {
+			ToAddresses: [to],
+		},
+		Message: {
+			Body: {
+				Text: { Data: text },
+			},
+			Subject: { Data: subject },
+		},
+		Source: from,
+	};
+
+	try {
+		await SES.sendEmail(params).promise();
+		return sendResponse200({});
+	} catch (error) {
+		console.log("error sending email ", error);
+		return sendResponse400({ message: "The email failed to send" });
+	}
+};
